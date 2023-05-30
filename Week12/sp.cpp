7a0e3643848203ab45bd4fd41d2324d401e9daa1
200582L <129168836+200582L@users.noreply.github.com>
2023-05-30 19:27:30 +0530

    Update sp.cpp

--- Week12/sp.cpp
+++ Week12/sp.cpp
@@ -1,66 +1,66 @@
 #include <iostream>
 #include <climits>
 
-#define V 6 
+#define V 6 // Number of vertices
 
-int findMinDistance(int distances[], bool visited[]) {
-    int min = INT_MAX, minIndex;
+int minTime(int Time[], bool visited[]) {
+    int min = INT_MAX, min_index;
 
     for (int v = 0; v < V; v++) {
-        if (!visited[v] && distances[v] <= min) {
-            min = distances[v];
-            minIndex = v;
+        if (!visited[v] && Time[v] <= min) {
+            min = Time[v];
+            min_index = v;
         }
     }
 
-    return minIndex;
+    return min_index;
 }
 
-void runDijkstra(int graph[V][V], int source) {
-    int distances[V]; 
-    bool visited[V]; 
+void dijkstra(int graph[V][V], int source) {
+    int Time[V]; // Store the shortest Time from source to each city
+    bool visited[V]; // Track visited cities
 
-    
+    // Initialize Time and visited array
     for (int i = 0; i < V; i++) {
-        distances[i] = INT_MAX;
+        Time[i] = INT_MAX;
         visited[i] = false;
     }
 
-    
-    distances[source] = 0;
+    // Time from source to itself is always 0
+    Time[source] = 0;
 
-    
+    // Find the shortest path for all cities
     for (int count = 0; count < V - 1; count++) {
-        int u = findMinDistance(distances, visited);
+        int u = minTime(Time, visited);
 
-        
+        // Mark the selected city as visited
         visited[u] = true;
 
-        
+        // Update Time of the adjacent cities of the selected city
         for (int v = 0; v < V; v++) {
-            if (!visited[v] && graph[u][v] && distances[u] != INT_MAX &&
-                distances[u] + graph[u][v] < distances[v]) {
-                distances[v] = distances[u] + graph[u][v];
+            if (!visited[v] && graph[u][v] && Time[u] != INT_MAX &&
+                Time[u] + graph[u][v] < Time[v]) {
+                Time[v] = Time[u] + graph[u][v];
             }
         }
     }
 
-    
-    std::cout << "Shortest distances from the source city " << source << ":\n";
+    // Print the shortest Time from the source to each city
+    std::cout << "Shortest Time from source city " << source << ":\n";
     for (int i = 0; i < V; i++) {
-        std::cout << "City " << i << ": " << distances[i] << "\n";
+        std::cout << "City " << i << ": " << Time[i] << "\n";
     }
 
-    
-    int totalDistance = 0;
+    // Calculate and print the average Time from the source to all other cities
+    int totalTime = 0;
     int numCities = V - 1; // Excluding the source city itself
     for (int i = 0; i < V; i++) {
         if (i != source) {
-            totalDistance += distances[i];
+            totalTime += Time[i];
         }
     }
-    double averageDistance = static_cast<double>(totalDistance) / numCities;
-    std::cout << "Average distance from the source city " << source << ": " << averageDistance << "\n";
+    double averageTime = static_cast<double>(totalTime) / numCities;
+    std::cout << "Average Time from source city " << source << ": " << averageTime << "\n";
     std::cout << "------------------------\n";
 }
 
@@ -74,10 +74,9 @@ int main() {
         {5, 0, 0, 20, 0, 0}
     };
 
-    
+    // Run Dijkstra's algorithm for each source city (0 to 5)
     for (int source = 0; source < V; source++) {
-        runDijkstra(graph, source);
+        dijkstra(graph, source);
     }
-
     return 0;
 }
